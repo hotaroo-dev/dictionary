bd6a05bac9c9a8718f7a6a064d198d5c67d4592c
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-09-21 21:06:12 +0700

    feat(*): add icons

---
+++ components/icons/Play.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 32 32"
+  >
+    <path
+      fill="currentColor"
+      d="M7 28a1 1 0 0 1-1-1V5a1 1 0 0 1 1.482-.876l20 11a1 1 0 0 1 0 1.752l-20 11A1 1 0 0 1 7 28Z"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CarbonPlayFilledAlt'
+}
+</script>
