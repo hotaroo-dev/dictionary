0fd29807631302b137de848af73cc71c1c611832
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-09-21 10:41:17 +0700

    feat(dev): add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import colors from 'tailwindcss/colors'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss'],
+  devtools: { enabled: true },
+  tailwindcss: {
+    config: {
+      theme: {
+        extends: {
+          colors: {
+            pimary: { ...colors.purple, DEFAULT: colors.purple[600] }
+          }
+        },
+        container: {
+          center: true,
+          paddding: {
+            DEFAULT: '1rem',
+            md: '1.5rem',
+            lg: '2rem'
+          }
+        }
+      }
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Dictionary</h1>
+    <h1 class="p-5 pt-5">Dictionary</h1>
   </div>
 </template>
